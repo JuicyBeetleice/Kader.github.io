2edcc7ddbd48e945bb402e44fd4d12991806369c
JuicyBeetleice <1159807923@qq.com>
2022-10-27 09:49:39 +0800

    cc

---
+++ link.js
@@ -0,0 +1,20 @@
+function move_menu(num)
+{
+	switch(num)
+
+	{
+
+	  case 1:window.location="aboutus.htm" ;break;
+
+	  case 2:window.location="product.htm" ;break;
+
+	  case 3:window.location="services.htm" ;break;
+
+	  case 4:window.location="readme.htm" ;break;
+
+	  case 5:window.location="1layout.htm" ;break;
+	  
+  	  case 6:window.location="2layout.htm" ;break;
+  	  
+  	}
+}
